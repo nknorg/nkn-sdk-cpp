2d0a964dd2d74969dd27049760a04f358faa4ab3
Moore <gdmmx@163.com>
2020-10-30 11:29:35 +0800

    Add universal json bean
    Signed-off-by: Moore <gdmmx@163.com>

---
+++ json/NKNCodec.cpp
@@ -0,0 +1,362 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <typeinfo>
+#include <exception>
+#include <stdexcept>
+// #include <cassert>
+
+#include <json/NKNCodec.h>
+
+using namespace rapidjson;
+using namespace NKN::JSON;
+
+Decoder::Decoder(const char* json) : mDocument(), mStack(), mError(false) {
+    mDocument = new GenericDocument<UTF8<> >();
+
+    // Parse and check
+    DOCUMENT->Parse(json);
+    errCode = DOCUMENT->GetParseError();
+    switch (errCode) {
+        case kParseErrorNone:   // valid json
+            break;
+        case kParseErrorDocumentRootNotSingular: // TODO: support multiple json obj from input
+        case kParseErrorDocumentEmpty:
+        case kParseErrorValueInvalid:   // invalid json
+        default: { // Others specific error
+            // TODO stderr, throw while #ifdef __EXCEPTIONS
+            DEBUG_JSON_CODEC("ParseError");
+            mError = true;
+            return;
+        }
+    }
+
+    mStack = new DecoderStack;
+    STACK->push(DecoderStackItem(DOCUMENT, DecoderStackItem::BeforeStart));
+}
+
+Decoder::Decoder(const std::string& json) : mDocument(), mStack(), mError(false) {
+    mDocument = new GenericDocument<UTF8<> >();
+
+    // Parse and check
+    DOCUMENT->Parse(json.c_str(), json.length());
+    errCode = DOCUMENT->GetParseError();
+    switch (errCode) {
+        case kParseErrorNone:   // valid json
+            break;
+        case kParseErrorDocumentRootNotSingular:    // TODO: support multiple json obj from input
+        case kParseErrorDocumentEmpty:
+        case kParseErrorValueInvalid:   // invalid json
+        default: {  // Others specific error
+            // TODO stderr, throw while #ifdef __EXCEPTIONS
+            DEBUG_JSON_CODEC("ParseError");
+            mError = true;
+            return;
+        }
+    }
+
+    mStack = new DecoderStack;
+    STACK->push(DecoderStackItem(DOCUMENT, DecoderStackItem::BeforeStart));
+}
+
+Decoder::~Decoder() {
+    delete DOCUMENT;
+    delete STACK;
+}
+
+// Archive concept
+Decoder& Decoder::StartObject() {
+    if (kParseErrorNone == errCode) {
+        if (CURRENT.IsObject() && TOP.state == DecoderStackItem::BeforeStart)
+            TOP.state = DecoderStackItem::Started;
+        else{
+            DEBUG_JSON_CODEC("Not Obj or Not start yet")
+            errCode = kParseErrorObjectMissCommaOrCurlyBracket;
+            mError = true;
+            // TODO stderr
+        }
+    }
+    return *this;
+}
+
+Decoder& Decoder::EndObject() {
+    if (kParseErrorNone == errCode) {
+        if (CURRENT.IsObject() && TOP.state == DecoderStackItem::Started)
+            Next();
+        else {
+            DEBUG_JSON_CODEC("Not Obj or Not start yet")
+            errCode = kParseErrorObjectMissCommaOrCurlyBracket;
+            mError = true;
+            // TODO stderr
+        }
+    }
+    return *this;
+}
+
+Decoder& Decoder::Member(const char* name) {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return *this;
+    }
+
+    if (CURRENT.IsObject() && TOP.state == DecoderStackItem::Started) {
+        Value::ConstMemberIterator memberItr = CURRENT.FindMember(name);
+        if (memberItr != CURRENT.MemberEnd())   // Member Found
+            STACK->push(DecoderStackItem(&memberItr->value, DecoderStackItem::BeforeStart));
+        // else // Not found
+            // mError = true;
+    } else {    // CURRENT not an object
+            DEBUG_JSON_CODEC("Not Obj or Not start yet")
+        errCode = kParseErrorObjectMissName;
+        // TODO stderr
+        mError = true;
+    }
+    return *this;
+}
+
+bool Decoder::HasMember(const char* name) const {
+    // if (!mError && CURRENT.IsObject() && TOP.state == DecoderStackItem::Started)
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return *this;
+    }
+
+    if (CURRENT.IsObject() && TOP.state == DecoderStackItem::Started)
+        return CURRENT.HasMember(name);
+
+    // TODO warning stderr
+    return false;
+}
+
+Decoder& Decoder::StartArray(size_t* size) {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return *this;
+    }
+
+    // NOT an array or array error
+    if (!CURRENT.IsArray() || TOP.state != DecoderStackItem::BeforeStart) {
+            DEBUG_JSON_CODEC("Not Array or Not start yet")
+        errCode = kParseErrorArrayMissCommaOrSquareBracket;
+        // TODO stderr
+        mError = true;
+        return *this;
+    }
+
+    TOP.state = DecoderStackItem::Started;
+    if (size)
+        *size = CURRENT.Size();
+
+    if (CURRENT.Empty())
+        TOP.state = DecoderStackItem::Closed;
+
+    const Value* value = &CURRENT[TOP.index];
+    STACK->push(DecoderStackItem(value, DecoderStackItem::BeforeStart));
+    return *this;
+}
+
+Decoder& Decoder::EndArray() {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return *this;
+    }
+
+    if (CURRENT.IsArray() && TOP.state == DecoderStackItem::Closed)
+        Next();
+    else{
+            DEBUG_JSON_CODEC("Not Array or Not closed")
+        errCode = kParseErrorArrayMissCommaOrSquareBracket;
+        mError = true;
+    }
+    return *this;
+}
+
+Decoder& Decoder::operator&(bool& b) {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return *this;
+    }
+
+    if (CURRENT.IsBool()) {
+        b = CURRENT.GetBool();
+        Next();
+    }
+    // else
+        // mError = true;
+    return *this;
+}
+
+Decoder& Decoder::operator&(unsigned& u) {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return *this;
+    }
+
+    if (CURRENT.IsUint()) {
+        u = CURRENT.GetUint();
+        Next();
+    }
+    // else
+        // mError = true;
+    return *this;
+}
+
+Decoder& Decoder::operator&(int& i) {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return *this;
+    }
+
+    if (CURRENT.IsInt()) {
+        i = CURRENT.GetInt();
+        Next();
+    }
+    // else
+        // mError = true;
+    return *this;
+}
+
+Decoder& Decoder::operator&(double& d) {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return *this;
+    }
+
+    if (CURRENT.IsNumber()) {
+        d = CURRENT.GetDouble();
+        Next();
+    }
+    // else
+        // mError = true;
+    return *this;
+}
+
+Decoder& Decoder::operator&(std::string& s) {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return *this;
+    }
+
+    if (CURRENT.IsString()) {
+        s = CURRENT.GetString();
+        Next();
+    }
+    // else
+        // mError = true;
+    return *this;
+}
+
+Decoder& Decoder::SetNull() {
+    // This function is for Encoder only.
+    mError = true;
+    return *this;
+}
+
+void Decoder::Next() {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+            DEBUG_JSON_CODEC("Had ParseError")
+        return;
+    }
+
+    assert(!STACK->empty());
+    STACK->pop();
+
+    if (!STACK->empty() && CURRENT.IsArray()) {
+        if (TOP.state == DecoderStackItem::Started) { // Otherwise means reading array item pass end
+            if (TOP.index < CURRENT.Size() - 1) {
+                const Value* value = &CURRENT[++TOP.index];
+                STACK->push(DecoderStackItem(value, DecoderStackItem::BeforeStart));
+            }
+            else
+                TOP.state = DecoderStackItem::Closed;
+        }
+        // else
+            // mError = true;
+    }
+}
+
+// #undef DOCUMENT
+// #undef STACK
+// #undef TOP
+// #undef CURRENT
+
+////////////////////////////////////////////////////////////////////////////////
+// Encoder
+
+Encoder::Encoder() : mWriter(), mStream() {
+    mStream = new StringBuffer;
+    mWriter = new PrettyWriter<StringBuffer>(*STREAM);
+}
+
+Encoder::~Encoder() {
+    delete WRITER;
+    delete STREAM;
+}
+
+const char* Encoder::GetString() const {
+    return STREAM->GetString();
+}
+
+Encoder& Encoder::StartObject() {
+    WRITER->StartObject();
+    return *this;
+}
+
+Encoder& Encoder::EndObject() {
+    WRITER->EndObject();
+    return *this;
+}
+
+Encoder& Encoder::Member(const char* name) {
+    WRITER->String(name, static_cast<SizeType>(strlen(name)));
+    return *this;
+}
+
+bool Encoder::HasMember(const char*) const {
+    // This function is for Decoder only.
+    assert(false);
+    return false;
+}
+
+Encoder& Encoder::StartArray(size_t*) {
+    WRITER->StartArray();
+    return *this;
+}
+
+Encoder& Encoder::EndArray() {
+    WRITER->EndArray();
+    return *this;
+}
+
+Encoder& Encoder::operator&(bool& b) {
+    WRITER->Bool(b);
+    return *this;
+}
+
+Encoder& Encoder::operator&(unsigned& u) {
+    WRITER->Uint(u);
+    return *this;
+}
+
+Encoder& Encoder::operator&(int& i) {
+    WRITER->Int(i);
+    return *this;
+}
+
+Encoder& Encoder::operator&(double& d) {
+    WRITER->Double(d);
+    return *this;
+}
+
+Encoder& Encoder::operator&(std::string& s) {
+    WRITER->String(s.c_str(), static_cast<SizeType>(s.size()));
+    return *this;
+}
+
+Encoder& Encoder::SetNull() {
+    WRITER->Null();
+    return *this;
+}
+
+// #undef STREAM
+// #undef WRITER
---
+++ json/NKNCodec.h
@@ -0,0 +1,188 @@
+#ifndef __JSON_CODEC_H__
+#define __JSON_CODEC_H__
+
+#include <cstddef>
+#include <string>
+#include <stack>
+
+#include <rapidjson/document.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/stringbuffer.h>
+
+#include <dtype/uBigInt.h>
+
+using namespace rapidjson;
+
+namespace NKN {
+namespace JSON {
+
+#ifdef DEBUG
+#define DEBUG_JSON_CODEC(msg) codec_dbg_t _dbg(static_cast<const void*>(this), __PRETTY_FUNCTION__, __LINE__, msg);
+#else
+#define DEBUG_JSON_CODEC(msg)
+#endif
+
+#ifdef DEBUG_JSON_CODEC
+class codec_dbg_t {
+public:
+    std::string _func;
+    unsigned int _line;
+    const void *ptr;
+    char *msg;
+
+    codec_dbg_t(const void *ptr, std::string f, unsigned int l, char *msg) : _func(f), _line(l), ptr(ptr), msg(msg) {
+        printf("%s:%d Entry:\t\tthis=%p %s\n", _func.c_str(), _line, ptr, msg);
+    }
+    ~codec_dbg_t() {
+        printf("%s:%d Exit :\t\tthis=%p\n", _func.c_str(), _line, ptr);
+    }
+};
+#endif // DEBUG_JSON_CODEC
+
+struct DecoderStackItem {
+    enum State {
+        BeforeStart,    //!< An object/array is in the stack but it is not yet called by StartObject()/StartArray().
+        Started,        //!< An object/array is called by StartObject()/StartArray().
+        Closed          //!< An array is closed after read all element, but before EndArray().
+    };
+
+    DecoderStackItem(const Value* value, State state) : value(value), state(state), index() {}
+
+    const Value* value;
+    State state;
+    SizeType index;   // For array iteration
+};
+
+typedef std::stack<DecoderStackItem> DecoderStack;
+
+#define DOCUMENT reinterpret_cast<Document*>(mDocument)
+#define STACK (reinterpret_cast<DecoderStack*>(mStack))
+#define TOP (STACK->top())
+#define CURRENT (*TOP.value)
+
+/// Represents a JSON reader which implements Archiver concept.
+class Decoder {
+public:
+    /// Constructor.
+    /**
+        \param json A non-const source json string for in-situ parsing.
+        \note in-situ means the source JSON string will be modified after parsing.
+    */
+    Decoder(const char* json);
+    Decoder(const std::string& json);
+
+    /// Destructor.
+    ~Decoder();
+
+    // Archive concept
+    operator bool() const { return !mError; }
+
+    virtual Decoder& StartObject();
+    virtual Decoder& Member(const char* name);
+    bool HasMember(const char* name) const;
+    virtual Decoder& EndObject();
+
+    virtual Decoder& StartArray(size_t* size = 0);
+    virtual Decoder& EndArray();
+
+    virtual Decoder& operator&(bool& b);
+    virtual Decoder& operator&(unsigned& u);
+    virtual Decoder& operator&(int& i);
+    virtual Decoder& operator&(double& d);
+    virtual Decoder& operator&(std::string& s);
+    template <size_t N>
+    Decoder& operator&(uBigInt<N> &bi);
+
+    virtual Decoder& SetNull();
+    virtual inline ParseErrorCode ErrCode() const { return errCode; }
+
+    static const bool IsReader = true;
+    static const bool IsWriter = !IsReader;
+
+private:
+    Decoder(const Decoder&);
+    Decoder& operator=(const Decoder&);
+
+    void Next();
+
+    // PIMPL
+    void* mDocument;              ///< DOM result of parsing.
+    void* mStack;                 ///< Stack for iterating the DOM
+    bool mError;                  ///< Whether an error has occurred.
+    ParseErrorCode errCode;
+};
+
+template <size_t N>
+Decoder& Decoder::operator&(uBigInt<N> &bi) {
+    if (kParseErrorNone != errCode) {   // Do nothing if Parse error
+        return *this;
+    }
+
+    if (CURRENT.IsString()) {
+        bi.FromHexString(CURRENT.GetString());
+        Next();
+    }
+    return *this;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Encoder
+
+#define WRITER reinterpret_cast<PrettyWriter<StringBuffer>*>(mWriter)
+#define STREAM reinterpret_cast<StringBuffer*>(mStream)
+
+class Encoder {
+public:
+    /// Constructor.
+    Encoder();
+
+    /// Destructor.
+    ~Encoder();
+
+    /// Obtains the serialized JSON string.
+    const char* GetString() const;
+
+    // Archive concept
+
+    operator bool() const { return true; }
+
+    virtual Encoder& StartObject();
+    virtual Encoder& Member(const char* name);
+    bool HasMember(const char* name) const;
+    virtual Encoder& EndObject();
+
+    virtual Encoder& StartArray(size_t* size = 0);
+    virtual Encoder& EndArray();
+
+    virtual Encoder& operator&(bool& b);
+    virtual Encoder& operator&(unsigned& u);
+    virtual Encoder& operator&(int& i);
+    virtual Encoder& operator&(double& d);
+    virtual Encoder& operator&(std::string& s);
+    template <size_t N>
+    Encoder& operator&(uBigInt<N> &bi);
+    virtual Encoder& SetNull();
+
+    static const bool IsReader = false;
+    static const bool IsWriter = !IsReader;
+
+private:
+    Encoder(const Encoder&);
+    Encoder& operator=(const Encoder&);
+
+    // PIMPL idiom
+    void* mWriter;      ///< JSON writer.
+    void* mStream;      ///< Stream buffer.
+};
+
+template <size_t N>
+Encoder& Encoder::operator&(uBigInt<N> &bi){
+    std::string s = bi.toHexString();
+    WRITER->String(s.c_str(), static_cast<SizeType>(s.size()));
+    return *this;
+}
+
+};  // namespace JSON
+};  // namespace NKN
+
+#endif // ARCHIVER_H__
