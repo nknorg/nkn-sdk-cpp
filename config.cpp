3ac9e4e2f537f25e11d273bd8dc470e36f997647
Moore <gdmmx@163.com>
2020-12-09 18:53:54 +0800

    supplement: missing config.cpp
    Signed-off-by: Moore <gdmmx@163.com>

---
+++ config.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+extern const vector<const string> DefaultRPCConfig;
+const vector<const string> DefaultRPCConfig = {{"http://seed.nkn.org:30003"}};
+
+// Copy construction from const DefaultRPCConfig and return a mutable RPCConfig
+inline vector<const string> GetDefaultRPCConfig() {
+    return vector<const string>(DefaultRPCConfig.begin(), DefaultRPCConfig.end());
+}
+
+const string GetRandomSeedRPCServerAddr(const vector<const string>& cfg = DefaultRPCConfig) {
+    random_device rd;
+    if (0 == cfg.size()) {    // TODO Empty cfg warning
+        cerr << "RPC config is Empty. " << cfg.size() << endl;
+        return "";
+    }
+    return cfg[ rd() % cfg.size() ];
+}
